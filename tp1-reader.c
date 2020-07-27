8a9a4b6ee922f52c412ff8171a67d7b6347020b9
Luciano Vittori <lucianovittori99@gmail.com>
2020-07-27 17:23:41 -0300

    Initial commit

---
+++ tp1-reader.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdint.h>
+
+#define FILE_NAME_DATA "Log.txt"
+#define FILE_NAME_SIGN "Sign.txt"
+#define FIFO_NAME "NamedFIFO"
+#define FIFO_ALREADY_EXIST -1
+#define BUFFER_SIZE 300
+
+int main(void)
+{
+    uint8_t inputBuffer[BUFFER_SIZE];
+    int32_t bytesRead, returnCode, fd;
+    FILE *f_data, *f_sign;
+    char character;
+
+    /* Create named fifo. -1 means already exists so no action if already exists */
+    if ((returnCode = mknod(FIFO_NAME, S_IFIFO | 0666, 0)) < FIFO_ALREADY_EXIST)
+    {
+        printf("Error creating named fifo: %d\n", returnCode);
+        exit(1);
+    }
+
+    /* Create or open if exist, for add info in text file */
+    f_data = fopen(FILE_NAME_DATA, "a+t");
+    f_sign = fopen(FILE_NAME_SIGN, "a+t");
+
+    /* Open named fifo. Blocks until other process opens it */
+    printf("waiting for writers...\n");
+    if ((fd = open(FIFO_NAME, O_RDONLY)) < 0)
+    {
+        printf("Error opening named fifo file: %d\n", fd);
+        exit(1);
+    }
+
+    /* open syscalls returned without error -> other process attached to named fifo */
+    printf("got a writer\n");
+
+    /* Loop until read syscall returns a value <= 0 */
+    do
+    {
+        /* read data into local buffer */
+        if ((bytesRead = read(fd, inputBuffer, BUFFER_SIZE)) == -1)
+        {
+            perror("read");
+        }
+        else
+        {
+            inputBuffer[bytesRead] = '\0';
+            printf("reader: read %d bytes: \"%s\"\n", bytesRead, inputBuffer);
+        }
+    } while (bytesRead > 0);
+
+    /* Close files */
+    fclose(f_data);
+    fclose(f_sign);
+
+    return 0;
+}
